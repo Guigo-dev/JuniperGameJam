c9f9a8216e4c5eee82054a3bd4b8150b6467a473
Guigo-dev <guipadiferreira@gmail.com>
2026-06-20 18:50:03 -0300

    bala com penetration

--- Scripts/bala.gd
+++ Scripts/bala.gd
@@ -1,14 +1,21 @@
 extends Area2D
 
 var direction
-var speed = 30
+@export var speed = 30
+@export var penetration = 1
 var target: Node2D
 
 
+func _ready() -> void:
+	add_to_group("bala")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	direction = global_position.direction_to(target.global_position)
 	global_position += direction * speed * delta * -1
-
-func _ready() -> void:
-	add_to_group("bala")
+	if(penetration == 0):
+		free()
+	
+func _on_area_entered(area: Area2D) -> void:
+	if(area.is_in_group("enemy")):
+		penetration -= 1
