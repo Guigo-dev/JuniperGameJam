90d27e2bb3a867108ae50c65cae6440ac3fa8dd4
yamagab <gsoler.yamamoto@gmail.com>
2026-06-23 20:49:09 -0300

    Loja com dictionary

--- Scripts/FasePrincipal/game_manager.gd
+++ Scripts/FasePrincipal/game_manager.gd
@@ -5,6 +5,9 @@ signal gun_changed(new_gun)
 @export var main_menu: PackedScene
 @export var souls : int = 0
 @export var current_gun : String = "default"
+var waveCounter: int = 0
+var remainingPowersKeys=[]
+
 
 
 func _ready() -> void:
@@ -14,3 +17,59 @@ func _process(delta: float) -> void:
 	if(Input.is_action_pressed("reset")):
 		get_tree().change_scene_to_packed(main_menu)
 		
+
+@export var powers =	{
+	0:{
+		"Name": "Ricochet",
+		"Des": "Ricochet bullets",
+		"Cost": 10
+	},
+	1:{
+		"Name": "Triple Shot",
+		"Des": "Triple the fun",
+		"Cost": 20
+	},
+	2:{
+		"Name": "Laser Aim",
+		"Des": "More precision",
+		"Cost": 30
+	},
+	3:{
+		"Name": "Auto-aim",
+		"Des": "Literal Aimbot",
+		"Cost": 40
+	},
+	4:{
+		"Name": "Spiral Bullet",
+		"Des": "Bullets can curve??",
+		"Cost": 50
+	},
+	5:{
+		"Name": "Heal",
+		"Des": "More HP",
+		"Cost": 60
+	},
+	6:{
+		"Name": "High Noon",
+		"Des": "Everything dies",
+		"Cost": 70
+	},
+	7:{
+		"Name": "Gold Gun",
+		"Des": "MONEY",
+		"Cost": 80
+	},
+	8:{
+		"Name": "Fire Gun",
+		"Des": "Fireball",
+		"Cost": 90
+	},
+	9:{
+		"Name": "Ice Gun",
+		"Des": "Achooo!",
+		"Cost": 100
+	},
+}
+func resetPool():
+	remainingPowersKeys=powers.keys()
+	remainingPowersKeys.shuffle()
--- Scripts/Menus/shop.gd
+++ Scripts/Menus/shop.gd
@@ -7,6 +7,16 @@ extends Control
 @onready var power2 : TextureButton = $HBoxContainer/Power2
 @onready var power3 : TextureButton = $HBoxContainer/Power3
 
+var powerKey = 0
+var shopSlots = 3
+
+func _ready() -> void:
+	for i in range(1,shopSlots+1):	
+		var currentPower = GameManager.powers[getNextPower()]
+		get_node("HBoxContainer/Power%s/Name" %i).text = currentPower["Name"]
+		get_node("HBoxContainer/Power%s/Description"%i).text = currentPower["Des"]
+		get_node("HBoxContainer/Power%s/Cost"%i).text = str(currentPower["Cost"])
+
 func _process(delta: float) -> void:
 	btn_hovered(power1)
 	btn_hovered(power2)
@@ -22,3 +32,19 @@ func 	btn_hovered(button: TextureButton):
 		start_tween(button,"scale", Vector2.ONE * tween_intensity, tween_duration)
 	else:
 		start_tween(button,"scale", Vector2.ONE, tween_duration)
+		
+func getNextPower():
+	if GameManager.remainingPowersKeys.is_empty():
+		GameManager.resetPool()
+	powerKey = GameManager.remainingPowersKeys.pop_back()
+	return powerKey	
+
+func _on_power_1_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_power_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_power_3_pressed() -> void:
+	pass # Replace with function body.
