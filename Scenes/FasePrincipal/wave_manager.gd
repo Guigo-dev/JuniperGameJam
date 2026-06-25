e2087ac047aa6eb58327ac7dd42c3dc88c4a8350
AndreLuisGrein <andreluisgrein@gmail.com>
2026-06-25 16:50:44 -0300

    Sistema de waves

---
+++ Scenes/FasePrincipal/wave_manager.gd
@@ -0,0 +1,131 @@
+extends Node
+
+@export var wave_time := 30.0
+
+
+
+var wave_enemies = {
+	1: {"A": 20, "B": 0, "C": 0},
+	2: {"A": 20, "B": 2, "C": 4},
+	3: {"A": 25, "B": 5, "C": 4},
+	4: {"A": 10, "B": 20, "C": 5},
+	5: {"A": 15, "B": 15, "C": 8},
+	6: {"A": 20, "B": 20, "C": 5},
+	7: {"A": 5, "B": 20, "C": 10},
+	8: {"A": 20, "B": 10, "C": 10},
+	9: {"A": 10, "B": 15, "C": 12},
+	10: {"A": 25, "B": 25, "C": 15}
+}
+
+var wave := 0
+
+var wave_timer := 0.0
+var wave_finished := false
+
+var enemies_alive := 0
+
+var spawn_events = []
+var current_event := 0
+
+var spawner_default
+var spawner_magro
+var spawner_gordo
+
+func _ready():
+
+	spawner_gordo = $"../Path2D/EnemySpawner"
+	spawner_magro = $"../Path2D2/EnemySpawner2"
+	spawner_default = $"../Path2D3/EnemySpawner3"
+
+	spawner_default.enemy_removed.connect(_on_enemy_removed)
+	spawner_magro.enemy_removed.connect(_on_enemy_removed)
+	spawner_gordo.enemy_removed.connect(_on_enemy_removed)
+
+	start_wave()
+
+func start_wave():
+
+	wave += 1
+
+
+	print("wave ", wave)
+
+	wave_timer = 0.0
+	wave_finished = false
+
+	current_event = 0
+
+	spawn_events.clear()
+
+	var data = wave_enemies[wave]
+
+	generate_events("A", data["A"])
+	generate_events("B", data["B"])
+	generate_events("C", data["C"])
+
+	spawn_events.sort_custom(
+		func(a,b):
+			return a.time < b.time
+	)
+
+func generate_events(enemy_type:String, amount:int):
+
+	if amount <= 0:
+		return
+
+	for i in range(amount):
+
+		spawn_events.append({
+			"time": randf_range(0.0, wave_time),
+			"type": enemy_type
+		})
+
+func _process(delta):
+
+	if wave_finished:
+
+		if enemies_alive <= 0:
+			start_wave()
+
+		return
+
+	wave_timer += delta
+
+	while current_event < spawn_events.size():
+
+		var event = spawn_events[current_event]
+
+		if wave_timer >= event.time:
+
+			spawn_enemy(event.type)
+
+			current_event += 1
+
+		else:
+			break
+
+	if wave_timer >= wave_time:
+
+		wave_finished = true
+
+
+func spawn_enemy(enemy_type:String):
+
+	match enemy_type:
+
+		"A":
+			spawner_default.spawn_enemy()
+
+		"B":
+			spawner_magro.spawn_enemy()
+
+		"C":
+			spawner_gordo.spawn_enemy()
+
+	enemies_alive += 1
+
+func _on_enemy_removed():
+	enemies_alive -= 1
+
+	if enemies_alive < 0:
+		enemies_alive = 0
--- Scripts/FasePrincipal/enemy_spawner.gd
+++ Scripts/FasePrincipal/enemy_spawner.gd
@@ -1,26 +1,34 @@
 extends Node2D
 
-
 @export var enemyScene: PackedScene
-@export var cooldown: float
-@onready var timer = $Timer
 @export var target: Node2D
+
 signal enemy_died
+signal enemy_removed
+
+func _on_enemy_removed():
+	enemy_removed.emit()
 
 func _ready() -> void:
-	timer.wait_time = cooldown
 	GameManager.gun_changed.connect(on_gun_changed)
 
+func spawn_enemy():
 
-func _on_timer_timeout() -> void:
+	if enemyScene == null:
+		return
+
 	var enemy = enemyScene.instantiate()
+
 	get_parent().add_child(enemy)
+
 	enemy.global_position = global_position
 	enemy.target = target
-	enemy.healthComponent.died.connect(on_enemy_died)
-	
-func on_enemy_died():
+
+	enemy.healthComponent.died.connect(_on_enemy_died)
+	enemy.tree_exited.connect(_on_enemy_removed)
+
+func _on_enemy_died():
 	enemy_died.emit()
-	
+
 func on_gun_changed(newGun):
 	target = newGun
