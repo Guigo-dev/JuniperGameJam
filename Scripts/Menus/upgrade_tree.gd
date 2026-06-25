a897b91204006e05d73e6e65297cce494a7155b0
Guigo-dev <guipadiferreira@gmail.com>
2026-06-25 08:21:41 -0300

    correcoes upgradeTree

--- Scripts/Menus/upgrade_tree.gd
+++ Scripts/Menus/upgrade_tree.gd
@@ -15,6 +15,18 @@ func _process(delta: float) -> void:
 		$BulletButton/DamageButton.disabled = true
 
 func atualizar_labels():
+	if(GameManager.gunStats["lifeUpgradeQtd"] == 3):
+		$GunButton/LifeStatButton.disabled = true
+	if(GameManager.gunStats["speedUpgradeQtd"] == 3):
+		$GunButton/VelStatButton.disabled = true
+	if(GameManager.gunStats["fire_rateUpgradeQtd"] == 3):
+		$GunButton/FireRateButton.disabled = true
+	if(GameManager.bulletStat["penetrationUpgradeQtd"] == 3):
+		$BulletButton/PenStatButton.disabled = true
+	if(GameManager.bulletStat["velocityUpgradeQtd"] == 3):
+		$BulletButton/BulletVelStatButton.disabled = true
+	if(GameManager.bulletStat["damageUpgradeQtd"] == 3):
+		$BulletButton/DamageButton.disabled = true
 	$GunButton/LifeStatButton/Level.text = "Level: " +str(GameManager.gunStats["lifeUpgradeQtd"])
 	$GunButton/VelStatButton/Level.text = "Level: " +str(GameManager.gunStats["speedUpgradeQtd"])
 	$GunButton/FireRateButton/Level.text = "Level: " +str(GameManager.gunStats["fire_rateUpgradeQtd"])
@@ -25,24 +37,18 @@ func atualizar_labels():
 	
 #emitindo sinais de upgrade de arma
 func _on_life_stat_button_pressed() -> void:
-	if(GameManager.gunStats["lifeUpgradeQtd"] == 2):
-		$GunButton/LifeStatButton.disabled = true
 	GameManager.gunStats["lifeUpgradeQtd"] += 1
 	atualizar_labels()
 	GameManager.XP -=1
 	GameManager.gun_stat_changed.emit(GameManager.GunStat.life)
 
 func _on_vel_stat_button_pressed() -> void:
-	if(GameManager.gunStats["speedUpgradeQtd"] == 2):
-		$GunButton/VelStatButton.disabled = true
 	GameManager.gunStats["speedUpgradeQtd"] += 1
 	atualizar_labels()
 	GameManager.XP -=1
 	GameManager.gun_stat_changed.emit(GameManager.GunStat.speed)
 
 func _on_fire_rate_button_pressed() -> void:
-	if(GameManager.gunStats["fire_rateUpgradeQtd"] == 2):
-		$GunButton/FireRateButton.disabled = true
 	GameManager.gunStats["fire_rateUpgradeQtd"] += 1
 	atualizar_labels()
 	GameManager.XP -=1
@@ -51,24 +57,18 @@ func _on_fire_rate_button_pressed() -> void:
 
 #emitindo sinais de upgrade de bala
 func _on_pen_stat_button_pressed() -> void:
-	if(GameManager.bulletStat["penetrationUpgradeQtd"] == 2):
-		$BulletButton/PenStatButton.disabled = true
 	GameManager.bulletStat["penetrationUpgradeQtd"] += 1
 	GameManager.XP -=1
 	atualizar_labels()
 	GameManager.bullet_stat_changed.emit(GameManager.BulletStat.penetration)
 
 func _on_bullet_vel_stat_button_pressed() -> void:
-	if(GameManager.bulletStat["velocityUpgradeQtd"] == 2):
-		$BulletButton/BulletVelStatButton.disabled = true
 	GameManager.bulletStat["velocityUpgradeQtd"] += 1
 	atualizar_labels()
 	GameManager.XP -=1
 	GameManager.bullet_stat_changed.emit(GameManager.BulletStat.velocity)
 
 func _on_damage_button_pressed() -> void:
-	if(GameManager.bulletStat["damageUpgradeQtd"] == 2):
-		$BulletButton/DamageButton.disabled = true
 	GameManager.bulletStat["damageUpgradeQtd"] += 1
 	atualizar_labels()
 	GameManager.XP -=1
