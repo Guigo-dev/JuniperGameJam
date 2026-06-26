a510b4e6e4928136d5f44f93a7929e6559221640
yamagab <gsoler.yamamoto@gmail.com>
2026-06-26 19:22:44 -0300

    tratando sobreposicao de armas

--- Scripts/Poderes/power_manager.gd
+++ Scripts/Poderes/power_manager.gd
@@ -19,8 +19,16 @@ func _on_upgrade_buyed():
 			else:
 				get_parent().get_child(0).add_child(powerNode)
 		elif GameManager.inventory[i]["Type"]== "Gun Modifier" or GameManager.inventory[i]["Type"] == "Gun":
+			if GameManager.inventory[i]["Type"]=="Gun":
+				if get_parent().has_node("ArmaDeFogo"):
+					get_parent().get_node("ArmaDeFogo").queue_free()
+				if get_parent().has_node("IceGun"):
+					get_parent().get_node("IceGun").queue_free()
+				if get_parent().has_node("GoldGun"):
+					get_parent().get_node("GoldGun").queue_free()
 			if has_node("/"+powerNode.name):
 				pass
+				
 			else:
 				add_sibling.call_deferred(powerNode)
 		else:
